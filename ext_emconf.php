a048b7a59e9073308b0b80c2f62f371f88157eb4
VladVilya <vladv161095@gmail.com>
2021-02-22 11:12:46 +0200

    [TASK] init extension commit

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'inteREST',
+    'description' => 'REST API for read/write access to database',
+    'version' => '1.0.0-alpha',
+    'category' => 'plugin',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.6-10.4.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'TTN\\Tea\\' => 'Classes/',
+        ],
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'TTN\\Tea\\Tests\\' => 'Tests/',
+        ],
+    ],
+];
