31644af2eebddf30244b11373df4ffb4d1ab67b0
VladVilya <vladv161095@gmail.com>
2021-04-01 12:41:59 +0300

    [WIP] temporary removed clear cache hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,5 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 (static function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][]
-        = \Pixelant\Interest\Hook\ClearCachePostProc::class . '->clearCachePostProc';
+
 })();
