771b968bcbe6b569767e97262ead9b37dc7c522e
Mathias Bolt Lesniak <mathias@lilio.com>
2021-10-06 15:09:14 +0200

    [TASK] TYPO3 v9 as possible dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.6-10.4.99',
+            'typo3' => '9.5.8-10.4.99',
         ],
     ],
     'autoload' => [
