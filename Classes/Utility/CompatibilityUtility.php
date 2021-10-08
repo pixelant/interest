8d1eb153276e94b0fb4d13b135901065208451af
mabolek <mathias@lilio.com>
2021-10-08 12:43:15 +0200

    [TASK] Add middleware compatibility

---
+++ Classes/Utility/CompatibilityUtility.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Pixelant\Interest\Utility;
+
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+/**
+ * Miscellaneous functions relating to compatibility with different TYPO3 versions
+ *
+ * @extensionScannerIgnoreFile
+ */
+class CompatibilityUtility
+{
+    /**
+     * Return the application context
+     *
+     * @return \TYPO3\CMS\Core\Core\ApplicationContext
+     */
+    public static function getApplicationContext()
+    {
+        if (self::typo3VersionIsLessThan('10.2')) {
+            return GeneralUtility::getApplicationContext();
+        }
+
+        return Environment::getContext();
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is less than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThan($version)
+    {
+        return self::getTypo3VersionInteger() < VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is less than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() <= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThan($version)
+    {
+        return self::getTypo3VersionInteger() > VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() >= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns the TYPO3 version as an integer
+     *
+     * @return int
+     */
+    public static function getTypo3VersionInteger()
+    {
+        return VersionNumberUtility::convertVersionNumberToInteger(VersionNumberUtility::getNumericTypo3Version());
+    }
+}
