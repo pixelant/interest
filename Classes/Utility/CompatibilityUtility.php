fb61905e28c3256e456b31172324226bffa4adaa
mabolek <mathias@lilio.com>
2021-10-13 12:29:40 +0200

    [TASK] Add PSR-14 support in T3 v9
    Uses signals and slots in a PSR-14-compatible way.

--- Classes/Utility/CompatibilityUtility.php
+++ Classes/Utility/CompatibilityUtility.php
@@ -3,8 +3,10 @@
 namespace Pixelant\Interest\Utility;
 
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 
 /**
  * Miscellaneous functions relating to compatibility with different TYPO3 versions
@@ -79,5 +81,70 @@ class CompatibilityUtility
     public static function getTypo3VersionInteger()
     {
         return VersionNumberUtility::convertVersionNumberToInteger(VersionNumberUtility::getNumericTypo3Version());
+    }
+
+    /**
+     * Dispatch an event as PSR-14 in TYPO3 v10+ and signal in TYPO3 v9.
+     *
+     * @param object $event
+     * @return object
+     */
+    public static function dispatchEvent(object $event): object
+    {
+        if (self::typo3VersionIsLessThan('10')) {
+            /** @var Dispatcher $signalSlotDispatcher */
+            $signalSlotDispatcher = GeneralUtility::makeInstance(Dispatcher::class);
+
+            $eventClassName = get_class($event);
+
+            $signalSlotDispatcher->dispatch(
+                $eventClassName,
+                self::classNameToSignalName($eventClassName),
+                [$event]
+            );
+
+            return $event;
+        }
+
+        /** @var EventDispatcher $eventDispatcher */
+        $eventDispatcher = GeneralUtility::makeInstance(EventDispatcher::class);
+
+        return $eventDispatcher->dispatch($event);
+    }
+
+    /**
+     * Register a PSR-14 event as a signal slot in TYPO3 v9.
+     *
+     * @param string $eventClassName
+     * @param string $eventHandlerClassName
+     */
+    public static function registerEventHandlerAsSignalSlot(string $eventClassName, string $eventHandlerClassName)
+    {
+        if (self::typo3VersionIsGreaterThanOrEqualTo('10')) {
+            return;
+        }
+
+        /** @var Dispatcher $signalSlotDispatcher */
+        $signalSlotDispatcher = GeneralUtility::makeInstance(Dispatcher::class);
+
+        $signalSlotDispatcher->connect(
+            $eventClassName,
+            self::classNameToSignalName($eventClassName),
+            $eventHandlerClassName,
+            '__invoke'
+        );
+    }
+
+    /**
+     * Returns "className" from "Foo\Bar\ClassName".
+     *
+     * @param string $className
+     * @return string
+     */
+    protected static function classNameToSignalName(string $className)
+    {
+        $explodedFqcn = explode('\\', $className);
+
+        return lcfirst(array_pop($explodedFqcn));
     }
 }
