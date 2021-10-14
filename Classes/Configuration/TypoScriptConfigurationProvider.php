f2ffd5ba8ea3c94daaaca7a3ec3fbc5541c4e1ff
mabolek <mathias@lilio.com>
2021-10-14 10:38:43 +0200

    [TASK][!!!] Use UserTS settings with dots

--- Classes/Configuration/TypoScriptConfigurationProvider.php
+++ Classes/Configuration/TypoScriptConfigurationProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pixelant\Interest\Configuration;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 
 /**
  * Class TypoScriptConfigurationProvider.
@@ -13,36 +12,16 @@ use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 class TypoScriptConfigurationProvider extends AbstractConfigurationProvider
 {
     /**
-     * @var ConfigurationManager
-     */
-    protected ConfigurationManager $configurationManager;
-
-    /**
-     * @param ConfigurationManager $configurationManager
-     */
-    public function injectConfigurationManager(ConfigurationManager $configurationManager): void
-    {
-        $this->configurationManager = $configurationManager;
-    }
-
-    /**
-     * Returns the settings read from the TypoScript.
+     * Returns the settings read from UserTS.
      *
      * @return array
-     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
      */
     public function getSettings(): array
     {
         if ($this->settings === null) {
-            $this->settings = [];
-
-            $typoScript = $GLOBALS['BE_USER']->getTsConfig();
-
-            if (isset($typoScript['tx_interest.'])) {
-                $this->settings = $typoScript['tx_interest.'];
-            }
+            $this->settings = $GLOBALS['BE_USER']->getTsConfig()['tx_interest.'] ?? [];
         }
 
-        return GeneralUtility::removeDotsFromTS($this->settings);
+        return $this->settings;
     }
 }
