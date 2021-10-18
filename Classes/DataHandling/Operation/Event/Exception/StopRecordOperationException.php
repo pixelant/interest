83f8516c8343c04bbc4f722a511a009ad41566fd
mabolek <mathias@lilio.com>
2021-10-18 16:12:16 +0200

    [TASK] Rename to StopRecordOperationException
    Defer doesn't always match.

--- Classes/DataHandling/Operation/Event/Exception/StopRecordOperationException.php
+++ Classes/DataHandling/Operation/Event/Exception/StopRecordOperationException.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 
 namespace Pixelant\Interest\DataHandling\Operation\Event\Exception;
 
-
+/**
+ * Thrown if record operations should stop. E.g. if the operation should be deferred.
+ */
 class StopRecordOperationException extends BeforeRecordOperationEventException
 {
 
