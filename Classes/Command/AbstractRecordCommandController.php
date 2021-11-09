5ed98cedcd57605ac34c48a08f2145a5e6f37d15
mabolek <mathias@lilio.com>
2021-11-09 15:52:44 +0100

    [TASK] Define AbstractRecordCommandController as abstract

--- Classes/Command/AbstractRecordCommandController.php
+++ Classes/Command/AbstractRecordCommandController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace Pixelant\Interest\Command;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,7 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  * Abstract command for handling operations with entrypoint and remoteId.
  */
-class AbstractRecordCommandController extends \Symfony\Component\Console\Command\Command
+abstract class AbstractRecordCommandController extends Command
 {
     /**
      * @inheritDoc
