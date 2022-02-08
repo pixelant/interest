d453636e1b8f1258559911c17b0fe511d974efa5
mabolek <mathias@lilio.com>
2022-02-08 11:59:37 +0100

    [BUGFIX] Fall back to operation data
    When retrieving an MM field configuration when handling foreign relation sorting, the record data is necessary to determine the correct configuration for typed records. If the record hasn't been persisted yet the data will fall back to the record operation's data.

--- Classes/DataHandling/Operation/Event/Handler/ForeignRelationSortingEventHandler.php
+++ Classes/DataHandling/Operation/Event/Handler/ForeignRelationSortingEventHandler.php
@@ -103,7 +103,7 @@ class ForeignRelationSortingEventHandler implements AfterRecordOperationEventHan
         $persistedRecordData = DatabaseUtility::getRecord(
             $recordOperation->getTable(),
             $recordOperation->getUid()
-        );
+        ) ?? $recordOperation->getData();
 
         $fieldConfigurations = [];
         foreach (array_keys($GLOBALS['TCA'][$recordOperation->getTable()]['columns']) as $fieldName) {
