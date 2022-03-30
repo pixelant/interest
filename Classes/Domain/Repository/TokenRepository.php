9e60f02253acdda84a2ac8cf82141638ca129bf1
mabolek <mathias@lilio.com>
2022-03-30 15:48:45 +0200

    [BUGFIX] Use fetchOne

--- Classes/Domain/Repository/TokenRepository.php
+++ Classes/Domain/Repository/TokenRepository.php
@@ -37,7 +37,7 @@ class TokenRepository extends AbstractRepository
                 )
             )
             ->execute()
-            ->fetchColumn();
+            ->fetchOne();
     }
 
     /**
