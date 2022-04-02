a423f9cb821b37f11bd24ac4770aebac76b52412
mabolek <mathias@lilio.com>
2022-04-02 19:49:53 +0200

    [TASK] Create empty unit test shell

---
+++ Tests/Unit/RequestHandler/AbstractRecordRequestHandlerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pixelant\Interest\Tests\Unit\RequestHandler;
+
+use Pixelant\Interest\RequestHandler\AbstractRecordRequestHandler;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * @covers AbstractRecordRequestHandler
+ */
+final class AbstractRecordRequestHandlerTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function correctlyCompilesData(): void
+    {
+    }
+}
