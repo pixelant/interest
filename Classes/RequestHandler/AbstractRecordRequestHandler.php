27201bed1730dd97539c1d0addbbd32df40798f8
mabolek <mathias@lilio.com>
2022-04-13 13:52:06 +0200

    [BUGFIX] Fix parsing of data arrays

--- Classes/RequestHandler/AbstractRecordRequestHandler.php
+++ Classes/RequestHandler/AbstractRecordRequestHandler.php
@@ -173,7 +173,7 @@ abstract class AbstractRecordRequestHandler extends AbstractRequestHandler
             if (!is_object($valueCopy[array_key_first($valueCopy)])) {
                 continue;
             } elseif (!is_array($valueCopy[array_key_first($valueCopy)]) || is_object($value)) {
-                $value = (array)$value;
+                $value = $this->convertObjectToArrayRecursive((array)$value);
             } elseif (is_array($value)) {
                 $value = $this->convertObjectToArrayRecursive($value);
             }
