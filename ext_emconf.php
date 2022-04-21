3360426481e39883e6b09b3f9f2f32f4f0bded2e
mabolek <mathias@lilio.com>
2022-04-21 16:37:53 +0200

    [BUGFIX] Disable XDebug by default in DDEV
    Also fixes the ext_emconf.php file for TER.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,11 @@
 <?php
+// phpcs:ignoreFile
 
-/** @noinspection PhpUndefinedVariableInspection */
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['interest'] = [
     'title' => 'Integration REST API',
-    'description' => 'REST and CLI API for adding, updating, and deleting records in TYPO3.'
-        . ' Tracks relations so records can be inserted in any order. Uses remote ID mapping so you don\'t have to'
-        . ' keep track of what UID a record has gotten after import. Data is inserted using backend APIs as if a real'
-        . ' human did it, so you can can inspect the record history and undo actions.',
-    'version' => '1.0.0-beta.1',
+    'description' => 'REST and CLI API for adding, updating, and deleting records in TYPO3. Tracks relations so records can be inserted in any order. Uses remote ID mapping so you don\'t have to keep track of what UID a record has gotten after import. Data is inserted using backend APIs as if a real human did it, so you can can inspect the record history and undo actions.',
+    'version' => '1.0.0',
+    'state' => 'beta',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
