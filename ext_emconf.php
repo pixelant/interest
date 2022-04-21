e2f7c8a04d2c9cbab7df3f3eeaf7c5b6bd39be3a
mabolek <mathias@lilio.com>
2022-04-21 15:15:43 +0200

    [TASK] Set version to 1.0.0-beta.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
         . ' Tracks relations so records can be inserted in any order. Uses remote ID mapping so you don\'t have to'
         . ' keep track of what UID a record has gotten after import. Data is inserted using backend APIs as if a real'
         . ' human did it, so you can can inspect the record history and undo actions.',
-    'version' => '1.0.0-alpha',
+    'version' => '1.0.0-beta.1',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
