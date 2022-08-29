8da75d2ddfaae33d0db19ed56da942a2366b2bba
mabolek <mathias@lilio.com>
2022-08-29 16:42:34 +0200

    [CLEANUP] Make PHPStan happy

--- Classes/Router/HttpRequestRouter.php
+++ Classes/Router/HttpRequestRouter.php
@@ -114,7 +114,7 @@ class HttpRequestRouter
 
         [$scheme, $token] = GeneralUtility::trimExplode(' ', $authorizationHeader, true);
 
-        if (strtolower($scheme ?? '') === 'bearer') {
+        if (is_string($scheme) && strtolower($scheme) === 'bearer') {
             $backendUserId = GeneralUtility::makeInstance(TokenRepository::class)
                 ->findBackendUserIdByToken($token);
 
