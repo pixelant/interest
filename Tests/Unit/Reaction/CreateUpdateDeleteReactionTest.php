a7ff78cda2e5b5c519845b91ecec91de5a255024
mabolek <git@mabolek.dev>
2023-09-17 18:14:57 +1200

    [TASK] Test for reactions in TYPO3 v12
    Also introduces additional requirements as a way to install and test possible/suggested requirements.

---
+++ Tests/Unit/Reaction/CreateUpdateDeleteReactionTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Pixelant\Interest\Tests\Unit\Reaction;
+
+use Pixelant\Interest\Reaction\CreateUpdateDeleteReaction;
+use Pixelant\Interest\Utility\CompatibilityUtility;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Reactions\Model\ReactionInstruction;
+use TYPO3\CMS\Reactions\Reaction\ReactionInterface;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class CreateUpdateDeleteReactionTest extends UnitTestCase
+{
+    protected ServerRequestInterface $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $stream = fopen('php://memory', 'r+');
+        fwrite($stream, '');
+        rewind($stream);
+
+        $this->request = new ServerRequest(
+            'http://www.example.com/typo3/reaction/70e05c6e-7025-4e87-932d-dc50ba50e121',
+            'POST',
+            $stream
+        );
+    }
+
+    public function addRequestHandlerToGeneralUtility(string $action)
+    {
+        $fqcn = 'Pixelant\\Interest\\RequestHandler\\' . $action . 'RequestHandler';
+
+        $mock = $this->createMock($fqcn);
+
+        $mock
+            ->expects($this->once())
+            ->method('handle')
+            ->willReturn($this->createMock(ResponseInterface::class));
+
+        GeneralUtility::addInstance($fqcn, $mock);
+    }
+
+    /**
+     * @test
+     */
+    public function reactRoutesToCorrectActionWithPayload()
+    {
+        if (
+            CompatibilityUtility::typo3VersionIsLessThan('12.0')
+            || !ExtensionManagementUtility::isLoaded('reactions')
+        ) {
+            return;
+        }
+
+        foreach (
+            [
+                'POST' => 'Create',
+                'PUT' => 'Update',
+                'PATCH' => 'CreateOrUpdate',
+                'DELETE' => 'Delete',
+            ] as $method => $action
+        ) {
+            $this->addRequestHandlerToGeneralUtility($action);
+
+            (new CreateUpdateDeleteReaction())->react(
+                $this->request,
+                [
+                    'method' => $method,
+                ],
+                $this->createMock(ReactionInstruction::class)
+            );
+        }
+    }
+}
