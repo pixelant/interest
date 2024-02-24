b4cddf3715b471f69690731ba2ca0220e338a845
mabolek <git@mabolek.dev>
2024-02-24 15:06:09 +0100

    [TASK] Raise version to 2.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 $EM_CONF['interest'] = [
     'title' => 'Integration REST API',
     'description' => 'REST and CLI API for adding, updating, and deleting records in TYPO3. Tracks relations so records can be inserted in any order. Uses remote ID mapping so you don\'t have to keep track of what UID a record has gotten after import. Data is inserted using backend APIs as if a real human did it, so you can can inspect the record history and undo actions.',
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'state' => 'stable',
     'category' => 'plugin',
     'constraints' => [
